74b3ab55824fc9c0c7c20d4a0ddce00bbd445da2
Knayder <knayder@gmail.com>
2022-05-14 11:05:09 +0200

    reverted changes xd

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
-from src.main import main
+from flask import Flask, jsonify, json
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+dictionary_path = 'assets/dictionary.json'
+
+dictionary = json.load(open(dictionary_path))
+
+@app.route('/dictionary')
+def get_dictionary():
+    return dictionary
+
+@app.route('/')
+def homepage():
+    return """
+    <h1>Hello heroku XDD</h1>
+    """
 
 if __name__ == '__main__':
-    print('App started')
-    main()
+    app.run(debug=True, use_reloader=True)
