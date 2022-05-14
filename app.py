3007d6914a7dfd0c98aca488bc48de2d51f843d4
Knayder <knayder@gmail.com>
2022-05-14 10:44:23 +0200

    moved to src

--- app.py
+++ app.py
@@ -1,19 +1,5 @@
-from flask import Flask, jsonify, json
-app = Flask(__name__)
-
-dictionary_path = 'assets/dictionary.json'
-
-dictionary = json.load(open(dictionary_path))
-
-@app.route('/dictionary')
-def get_dictionary():
-    return dictionary
-
-@app.route('/')
-def homepage():
-    return """
-    <h1>Hello heroku XDD</h1>
-    """
+from src.main import main
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    print('App started')
+    main()
