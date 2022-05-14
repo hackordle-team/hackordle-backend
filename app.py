c1aa6fad863b5a74e16be50285ff3c1370454cde
Adam Dyda <adamdyda@Adams-MacBook-Air.local>
2022-05-14 11:27:23 +0200

    added quizes endpoint

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 from flask import Flask, jsonify, json
 from flask_cors import CORS
+from matplotlib.pyplot import quiver
 
 app = Flask(__name__)
 CORS(app)
 
 dictionary_path = 'assets/dictionary.json'
+quizes_path = 'assets/quizes.json'
 
 dictionary = json.load(open(dictionary_path))
+quizes = json.load(open(quizes_path))
 
 @app.route('/dictionary')
 def get_dictionary():
     return dictionary
+
+@app.route('/quizes')
+def get_quizes():
+    return quizes
 
 @app.route('/')
 def homepage():
