8ebb882aef8d3e6b71b0056dccdb36ef677330d0
Knayder <knayder@gmail.com>
2022-05-14 12:28:56 +0200

    Added merging dictionaries

--- src/dictionary.py
+++ src/dictionary.py
@@ -1,8 +1,20 @@
 from flask import json
+from src.word_otd import custom_dictionary
+
 
 dictionary_path = 'assets/dictionary.json'
-dictionary = json.load(open(dictionary_path))
 
+
+def merge_dictionaries():
+    dictionary = json.load(open(dictionary_path))
+    merged_dictionary = list(set(dictionary['dictionary'] + custom_dictionary['dictionary']))
+    
+    return {
+        "dictionary": sorted(merged_dictionary)
+    }
+
+
+dictionary = merge_dictionaries()
 
 def get_dictionary():
     return dictionary
